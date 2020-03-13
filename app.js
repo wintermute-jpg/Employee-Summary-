641831d67a5413b391740bbefcd68810d798fe48
Gabe Young <gabriel_young@emerson.edu>
2020-03-13 18:47:37 -0400

    finalized roster creation process

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const render = require("./lib/htmlRenderer");
 const rosterInfo = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+createManager();
 
 //create manager function 
 function createManager() {
@@ -39,12 +40,11 @@ function createManager() {
     ]).then((response) => {
         const newManager = new Manager (response.name, response.id, response.email, response.officeID);
         console.log(newManager);
-        addEmployee();
         rosterInfo.push(newManager);
+        addEmployee(); 
     })
 };
 
-createManager();
 //Functions:
 //Create Employee
 function addEmployee(){
@@ -95,7 +95,7 @@ function createEngineer() {
           name: "github",
       }
   ]).then((response) => {
-      const newEngineer = new Engineer (response.name, response.id, response.email, response.officeID);
+      const newEngineer = new Engineer (response.name, response.id, response.email, response.github);
       console.log(newEngineer);
       rosterInfo.push(newEngineer);
       addEmployee();
@@ -126,38 +126,29 @@ function createIntern() {
           name: "school",
       }
   ]).then((response) => {
-      const newIntern = new Intern (response.name, response.id, response.email, response.officeID);
+      const newIntern = new Intern (response.name, response.id, response.email, response.school);
       console.log(newIntern);
       rosterInfo.push(newIntern);
       addEmployee();
   })
 };
 //Build Roster Function
-
 function buildRoster(){
+  //console.log(rosterInfo);
+   var rendered = render(rosterInfo);
+   console.log(rendered);
+    outputPath.push(rendered);
   //function to create ending file
-  TODO://need to create an if statement to handle if a file exists or not
-  TODO: //reference line 8-10 in build roster function
-  TODO://finish render roster.
+  //need to create an if statement to handle if a file exists or not
+   //reference line 8-10 in build roster function
+  //finish render roster.
 
-    fs.writeFileSync();
-} 
+    //fs.writeFileSync();
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
